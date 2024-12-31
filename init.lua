3dcea598d14b6287acc03e7890f43f405ed6e58e
rsvn <rizwanshaz2004@gmail.com>
2024-12-31 15:44:28 +0530

    first commit

---
+++ init.lua
@@ -0,0 +1,5 @@
+require('rsvn.lazy')
+require('rsvn.remap')
+
+require('rsvn.base')
+require('rsvn.options')
---
+++ lua/rsvn/remap.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader= " "
+vim.keymap.set("n","<leader>pv",vim.cmd.Ex)
+-- Press jk fast to exit insert mode 
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("i", "kj", "<ESC>")
+
+
