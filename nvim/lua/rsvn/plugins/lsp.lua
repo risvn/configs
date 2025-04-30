e325b49684f6a5b8c3777bf8d1f07b264e69b22d
rsvn <rizwanshaz2004@gmail.com>
2025-04-30 11:06:42 +0530

    lsp-setup

--- nvim/lua/rsvn/plugins/lsp.lua
+++ nvim/lua/rsvn/plugins/lsp.lua
@@ -1,47 +1,83 @@
 return {
-  -- LSP base plugins
+  -- LSP Setup
   {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate",
-    config = function()
-      require("mason").setup()
-    end
-  },
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip", -- optional for snippet support
+      "saadparwaiz1/cmp_luasnip",
+    },
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "mason.nvim" },
     config = function()
+      -- Mason setup
+      require("mason").setup()
+
       require("mason-lspconfig").setup({
         ensure_installed = {
           "pyright",
-          "clangd",
           "bashls",
+          "clangd",
           "lua_ls",
         },
       })
-    end
-  },
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "mason-lspconfig.nvim" },
-    config = function()
-      local lspconfig = require("lspconfig")
+      -- LSP client capabilities with cmp support
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Optional: common settings for all servers
-      local on_attach = function(client, bufnr)
-        print("LSP attached:", client.name)
+      -- Optional on_attach
+      local on_attach = function(_, bufnr)
+        local opts = { buffer = bufnr }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
       end
 
+      -- Setup all LSPs
       require("mason-lspconfig").setup_handlers({
         function(server_name)
-          lspconfig[server_name].setup({
+          require("lspconfig")[server_name].setup({
             on_attach = on_attach,
+            capabilities = capabilities,
           })
         end,
       })
-    end
-  },
+
+      -- nvim-cmp setup
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      cmp.setup({
+        completion = {
+          autocomplete = false, -- disable autosuggest while typing
+        },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(), -- trigger manually
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+
+    end,
+  }
 }
 
