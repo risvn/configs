d241823c573ba421618889eccdd889087bf090ec
rsvn <rizwanshaz2004@gmail.com>
2025-05-08 21:51:49 +0530

    remaped_key

--- nvim/lua/rsvn/remap.lua
+++ nvim/lua/rsvn/remap.lua
@@ -3,5 +3,7 @@ vim.keymap.set("n","<leader>x",vim.cmd.Ex)
 -- Press jk fast to exit insert mode 
 vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("i", "kj", "<ESC>")
+vim.keymap.set('n', '<C-b>', '<C-v>', { noremap = true })
+
 
 
